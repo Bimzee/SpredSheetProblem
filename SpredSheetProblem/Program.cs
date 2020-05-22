942f12d09f7d45ad1f6c88e2eeb624973872182b
Bimal Varghese <bimalvv2005@gmail.com>
2020-05-22 23:27:39 +0530

    Updated code read input from console

--- SpredSheetProblem/Program.cs
+++ SpredSheetProblem/Program.cs
@@ -6,32 +6,37 @@ namespace SpredSheetProblem
     {
         static void Main(string[] args)
         {
-
-            var n = 27;
-            for (int i = 1; i < 703; i++)
-            {
-                n = i;
+            Console.WriteLine("Please enter a number");
+            var n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i < 702; i++)
+            //{
+            //    n = i;
 
                 var n1 = n > 702 ? n % 702 : n;
+                if (n1 == 0)
+                {
+                    n -= 1;
+                    n1 = (n > 702 ? n % 702 : n) + 1;
+                }
 
                 var t = getSpreadsheetNotation(n1);
-                //var letters = ColumnIndexToColumnLetter(n1);
+                var letters = ColumnIndexToColumnLetter(n1);
 
                 var lineNum = n / 702;
                 var num = (lineNum > 0 ? lineNum : 0) + 1;
                 Console.WriteLine("-----------");
 
-                //Console.WriteLine(num + letters);
+                Console.WriteLine(num + letters);
                 Console.WriteLine("\t" + num + t);
                 Console.WriteLine("-----------");
-            }
+            //}
 
         }
         public static string getSpreadsheetNotation(long n)
         {
             //Rows 1 - 10^9
             //Columns : A - ZZ (702)
-            
+
             string result = "";
             if (n <= 26)
             {
@@ -53,19 +58,19 @@ namespace SpredSheetProblem
         }
 
 
-        //static string ColumnIndexToColumnLetter(int colIndex)
-        //{
-        //    int div = colIndex;
-        //    string colLetter = String.Empty;
-        //    int mod = 0;
+        static string ColumnIndexToColumnLetter(int colIndex)
+        {
+            int div = colIndex;
+            string colLetter = String.Empty;
+            int mod = 0;
 
-        //    while (div > 0)
-        //    {
-        //        mod = (div - 1) % 26;
-        //        colLetter = (char)(65 + mod) + colLetter;
-        //        div = (int)((div - mod) / 26);
-        //    }
-        //    return colLetter;
-        //}
+            while (div > 0)
+            {
+                mod = (div - 1) % 26;
+                colLetter = (char)(65 + mod) + colLetter;
+                div = (int)((div - mod) / 26);
+            }
+            return colLetter;
+        }
     }
 }
